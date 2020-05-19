94e208d60aa6567ff22e3ed18a5be091c529fd25
SerQuicky <Michael.frank96@web.de>
2020-05-19 20:14:51 +0200

    Add Campaign-Start to Templates

--- src/Templates.elm
+++ src/Templates.elm
@@ -1,6 +1,6 @@
 module Template exposing (startMenuTemplate, loadSavesTemplate)
 
-import Html exposing (Html, button, div, span, text)
+import Html exposing (Html, button, div, span, text, input, select, option, img)
 import Html.Attributes as HtmlAttr
 import Html.Events exposing (onClick)
 import List exposing (..)
@@ -11,7 +11,7 @@ import String exposing (..)
 startMenuTemplate : Html Msg
 startMenuTemplate  =
     div [ HtmlAttr.class "main-container" ]
-    [ addStylesheet "link" "styles.css"
+    [ addStylesheet "link" ".assets/styles/template_styles.css"
     , addStylesheet "link" "//fonts.googleapis.com/css?family=MedievalSharp"
     , div [ HtmlAttr.class "start-logo-container" ] [ span [] [ Html.text "Logo" ] ]
     , div [ HtmlAttr.class "start-container" ]
@@ -26,11 +26,41 @@ startMenuTemplate  =
     ]
 
 
+startCampaign : Html Msg 
+startCampaign =
+    div [ HtmlAttr.class "main-container" ]
+    [ addStylesheet "link"  ".assets/styles/template_styles.css"
+    , addStylesheet "link" "//fonts.googleapis.com/css?family=MedievalSharp"
+    , div [ HtmlAttr.class "start-logo-container" ] [ img [HtmlAttr.src "https://i.ibb.co/hRgjXkq/logo.png"] [] ]
+    , div [ HtmlAttr.class "campaign-container" ]
+        [ div [ HtmlAttr.class "start-header" ]
+            [ span [ HtmlAttr.class "start-header-text" ] [ Html.text "M'lord, what are your campaign-settings?" ] ]
+        , div [ HtmlAttr.class "campaign-actions" ]
+            [ div [ HtmlAttr.class "campaign-name-container"] 
+                [
+                    span [HtmlAttr.class "campaign-name"] [Html.text "Name:"]
+                    , input [HtmlAttr.class "campaign-input"] []
+                ]
+            , div [ HtmlAttr.class "campaign-select-container"] 
+                [
+                    span [HtmlAttr.class "campaign-name"] [Html.text "Lords:"]
+                    , select [HtmlAttr.class "campaign-select"] (lordsToOption (List.range 2 4))
+                ]
+            , div [ HtmlAttr.class "campaign-buttons-container"] 
+                [
+                    div [] [ button [ onClick ShowMap, HtmlAttr.class "start-buttons start-campaign-button" ] [ span [] [ Html.text "Start Campaign" ] ] ]
+                ]
+            ]
+        , div [] [ button [ onClick ShowLogin, HtmlAttr.class "back-btn" ] [ span [] [ Html.text "Back" ] ] ]
+        ]
+    ]
+
+
 -- Saved loads template with some example data
 loadSavesTemplate : Html Msg
 loadSavesTemplate =
     div [ HtmlAttr.class "main-container" ]
-    [ addStylesheet "link" "styles.css"
+    [ addStylesheet "link"  ".assets/styles/template_styles.css"
     , addStylesheet "link" "//fonts.googleapis.com/css?family=MedievalSharp"
     , div [ HtmlAttr.class "start-logo-container" ] [ span [] [ Html.text "Logo" ] ]
     , div [ HtmlAttr.class "save-loads-container" ]
@@ -61,6 +91,8 @@ loadSavesTemplate =
         , div [] [ button [ onClick ShowLogin, HtmlAttr.class "back-btn" ] [ span [] [ Html.text "Back" ] ] ]
         ]
     ]
+
+
 
 
 addStylesheet : String -> String -> Html Msg 
