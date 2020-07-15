a489dde9b1868740b5b1598b17dc4775dd793c0b
Florian <florian.kohrs@web.de>
2020-07-15 15:03:21 +0200

    Begin  AI

--- src/AI/AI.elm
+++ src/AI/AI.elm
@@ -1,5 +1,6 @@
 module AI exposing (..)
 
+import Dict
 import Entities
 import PathAgent
 import Pathfinder
@@ -7,74 +8,93 @@ import Types
 import Vector
 
 
-x =
-    0
+type alias AI =
+    { lord : Entities.Lord, strategy : ActionMultipliers }
 
 
-
-{-
-   type alias AI =
-       { lord : Entities.Lord, strategy : Strategy, nav : Pathfinder.NavigatableMap }
+type alias ActionMultipliers =
+    { siegeM : Float, defendM : Float, battleM : Float, growArmyM : Float }
 
 
-   type Strategy
-       = Siege
-       | Defend
-       | Battle
-       | BigArmy
+type Strategy
+    = Siege
+    | Defend
+    | Battle
+    | BigArmy
 
 
-   type alias Action =
-       { actionType : ActionType, urgency : Float }
+type alias Action =
+    { actionType : ActionType, urgency : Float }
 
 
-   type ActionType
-       = DefendSettlement Entities.Settlement
-       | Attacklord Entities.Lord
-       | GoToSettlement SettlementInteraction
-       | SiegeSettlement Entities.Settlement
+type ActionType
+    = DefendSettlement Entities.Settlement
+    | Attacklord Entities.Lord
+    | GoToSettlement SettlementInteraction
+    | SiegeSettlement Entities.Settlement
 
 
-   type SettlementInteraction
-       = StationTroops
-       | TakeTroops
-       | ImproveBuilding
-       | HireTroops
+type SettlementInteraction
+    = StationTroops
+    | TakeTroops
+    | ImproveBuilding
+    | HireTroops
 
 
-   type alias SettlementStatus =
-       { isSiegedBy : Maybe Entities.Lord, turnsTillReached : Float, settlement : Entities.Settlement }
+type AiRoundActions
+    = EndRound
+    | GoSomeWhere Vector.Point
+    | DoSomething BasicAction
 
 
-   type alias EnemyStatus =
-       { strengthDiff : Float, turnsTillReached : Float, settlement : Entities.Settlement }
+type BasicAction
+    = AttackLord Entities.Lord
+    | HireTroops (Dict.Dict Entities.TroopType Int) Entities.Settlement
+    | SwapTroops (Dict.Dict Entities.TroopType Int) Entities.Settlement
+    | SiegeSettlement Entities.Settlement
 
 
-   settlementStates : AI -> List Entities.Lord -> List SettlementStatus
-   settlementStates ai others =
-       List.map
-           (\s ->
-               SettlementStatus (settlementSiegedBy s others)
-                   (ai.getDistance s.entity.position)
-                   s
-           )
-           ai.lord.land
+type alias SettlementStatus =
+    { isSiegedBy : Maybe Entities.Lord, turnsTillReached : Float, settlement : Entities.Settlement }
 
 
-   settlementSiegedBy : Entities.Settlement -> List Entities.Lord -> Maybe Entities.Lord
-   settlementSiegedBy s =
-       List.foldr
-           (\l r ->
-               if l.entity.position == s.entity.position then
-                   Just l
+type alias EnemyStatus =
+    { strengthDiff : Float, turnsTillReached : Float, settlement : Entities.Settlement }
 
-               else
-                   r
-           )
-           Nothing
+getAiAction : AI -> List Entities.Lord -> AiRoundActions
+getAiAction ai lordEntitiesList =
+    let
+        remainingMovement = Entities.getLordRemainingMovement ai.lord
+    in
+    if remainingMovement == 0 then
+      EndRound
+    else
 
 
-   strengthDiff : Entities.Lord -> Entities.Lord -> Float
-   strengthDiff attacker defender =
-       0.5
--}
+settlementStates : AI -> List Entities.Lord -> List SettlementStatus
+settlementStates ai others =
+    List.map
+        (\s ->
+            SettlementStatus (settlementSiegedBy s others)
+                (ai.getDistance s.entity.position)
+                s
+        )
+        ai.lord.land
+
+
+settlementSiegedBy : Entities.Settlement -> List Entities.Lord -> Maybe Entities.Lord
+settlementSiegedBy s =
+    List.foldr
+        (\l r ->
+            if l.entity.position == s.entity.position then
+                Just l
+
+            else
+                r
+        )
+        Nothing
+
+
+strengthDiff : Entities.Lord -> Entities.Lord -> Float
+strengthDiff attacker defender =
+    0.5
