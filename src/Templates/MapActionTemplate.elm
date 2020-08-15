8c8adad1b0b67a33075cdc200db81387e263e11d
SerQuicky <Michael.frank96@web.de>
2020-08-15 15:07:58 +0200

    Add first implementation of the troop overview

--- src/Templates/MapActionTemplate.elm
+++ src/Templates/MapActionTemplate.elm
@@ -1,5 +1,8 @@
 module Templates.MapActionTemplate exposing (..)
 
+import Dict
+import DictExt
+import Entities.Model
 import Html exposing (Html, button, div, span, text)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
@@ -8,8 +11,12 @@ import MapAction.Model
 import MapAction.SubModel
 import MapAction.Viewer
 import Msg
+import Templates.HelperTemplate as Helper
+import Troops
 import Vector
-
+import OperatorExt
+import Faction exposing (Faction)
+import Entities.Model
 
 {-| Returns the layout for the map actions, in dependence to the chosen point (in the model)
 
@@ -17,8 +24,8 @@ import Vector
     @param {MapDrawer.InteractableMapSVG}: Takes a dict with all possible actions
 
 -}
-generateMapActionTemplate : Maybe Vector.Point -> MapAction.Model.InteractableMapSVG -> Html Msg.Msg
-generateMapActionTemplate p dict =
+generateMapActionTemplate : Maybe Vector.Point -> Entities.Model.Lord -> MapAction.Model.InteractableMapSVG -> Html Msg.Msg
+generateMapActionTemplate p player dict =
     let
         actions =
             case p of
@@ -28,8 +35,11 @@ generateMapActionTemplate p dict =
                 Just x ->
                     MapAction.actionsOnPoint x dict
     in
-    div [ Html.Attributes.class "map-action-menu" ]
-        (span [] [ Html.text "Map Actions" ] :: List.map generateMapActionButtons actions)
+    div []
+        [ div [ Html.Attributes.class "map-action-menu" ]
+            (span [] [ Html.text "Map Actions" ] :: List.map generateMapActionButtons actions)
+        , generateTroopOverview player.entity.faction actions
+        ]
 
 
 {-| Function for map, that displays a button for each possible action
@@ -40,3 +50,47 @@ generateMapActionTemplate p dict =
 generateMapActionButtons : MapAction.SubModel.MapTileMsg -> Html Msg.Msg
 generateMapActionButtons svga =
     button [ onClick (Msg.MapTileAction svga) ] [ span [] [ Html.text (MapAction.Viewer.mapTileMsgToToolTip svga) ] ]
+
+
+generateTroopOverview : Faction -> List MapAction.SubModel.MapTileMsg -> Html Msg.Msg
+generateTroopOverview pf actions =
+    div [ Html.Attributes.class "map-troop-overview" ]
+        ((span [] [ Html.text "Enemy troops on map tile" ]
+            :: DictExt.foldlOverKeys
+                (\k v r -> Helper.troopToHtml (Troops.intToTroopType k) v "mini-lord-troop-container" :: r)
+                (\k r -> Helper.troopToHtml (Troops.intToTroopType k) 0 "mini-lord-troop-container" :: r)
+                []
+                (combineTroopDicts Troops.emptyTroops actions (\f -> f /= pf))
+                Troops.troopKeyList
+        )
+        ++ (span [] [ Html.text "Your troops on map tile" ]
+                :: DictExt.foldlOverKeys
+                    (\k v r -> Helper.troopToHtml (Troops.intToTroopType k) v "mini-lord-troop-container" :: r)
+                    (\k r -> Helper.troopToHtml (Troops.intToTroopType k) 0 "mini-lord-troop-container" :: r)
+                    []
+                    (combineTroopDicts Troops.emptyTroops actions (\f -> f == pf)) 
+                    Troops.troopKeyList
+           ))
+
+
+combineTroopDicts : Troops.Army -> List MapAction.SubModel.MapTileMsg -> (Faction -> Bool) -> Troops.Army
+combineTroopDicts a l ff =
+    case l of
+        [] ->
+            a
+
+        x :: xs ->
+            combineTroopDicts (Troops.mergeTroops a (getEntityTroopsByMapTile x ff)) xs ff
+
+
+getEntityTroopsByMapTile : MapAction.SubModel.MapTileMsg -> (Faction -> Bool) -> Troops.Army
+getEntityTroopsByMapTile svga ff =
+    case svga of
+        MapAction.SubModel.LordMsg _ lord ->
+            OperatorExt.ternary (ff lord.entity.faction) lord.entity.army Troops.emptyTroops
+
+        MapAction.SubModel.SettlementMsg _ settlement ->
+            OperatorExt.ternary (ff settlement.entity.faction) settlement.entity.army Troops.emptyTroops
+
+        _ ->
+            Troops.emptyTroops
