e2a0019b808f390e130812c06bcd3ac473078247
SerQuicky <Michael.frank96@web.de>
2020-08-18 22:10:06 +0200

    Implement building icons in the overview and the wiki

--- src/Templates/SettlementTemplate.elm
+++ src/Templates/SettlementTemplate.elm
@@ -234,7 +234,9 @@ generateRecruitTroopContainer t aAmount sAmount s l =
 displayBuildingComponents : ( Building.Building, Entities.Model.Lord, Entities.Model.Settlement ) -> Html Msg.Msg
 displayBuildingComponents ( b, l, s ) =
     div [ Html.Attributes.class "settlement-building-component" ]
-        [ div [] []
+        [ div [Html.Attributes.class "building-icon-container"] [
+            img [ src ("./assets/images/buildings/" ++  b.name ++ ".png") ] []
+        ]
         , div [] [ span [] [ Html.text b.name ] ]
         , div [ Html.Attributes.class "tooltip" ]
             [ img [ Html.Attributes.class "info-icon", src "./assets/images/general/info.png" ] []
